58b20131a4e24de175a0e8401d06ab8419181455
Lisa <bilberry2009@yandex.ru>
2022-08-15 21:11:48 +0800

    exs19

--- exs19/Program.cs
+++ exs19/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+Console.WriteLine("Введите пятизначное число: ");
+int number=Convert.ToInt32(Console.ReadLine());
+int number1=number%10;
+Console.WriteLine(number1);
+int number2=number%100/10;
+Console.WriteLine(number2);
+int number3=number%1000/100;
+Console.WriteLine(number3);
+int number4=number/1000%10;
+Console.WriteLine(number4);
+int number5=number/1000/10;
+Console.WriteLine(number5);
+if (number5==number1|| number2==number4)
+{
+    Console.WriteLine("Число является палиандромом");
+}
+else
+{
+    Console.WriteLine("Число  не является палиандромом");
+}
